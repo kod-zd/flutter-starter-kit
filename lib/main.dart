86c8d9d1c3c7792676eb40f0ba53b2845a92be60
Ra <97934820+kod-zd@users.noreply.github.com>
2026-05-24 16:46:06 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,31 +7,25 @@ import 'views/pages/unknown_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ZIO',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // Define the default brightness and colors
-        // brightness: Brightness.dark,
         primaryColor: Styles.appPrimaryColor,
         accentColor: Styles.appAccentColor,
         brightness: Brightness.light,
-
-        // for drawer color
         canvasColor: Styles.appCanvasColor,
-
-        // Define the default font family.
         fontFamily: 'Montserrat',
-
-        // Define the default TextTheme. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
         textTheme: Styles.appTextTheme,
       ),
-      initialRoute: '/splash',
+
+      // 👇 هذا أهم شيء
+      initialRoute: '/zio',
+
       routes: appRoutes,
+
       onUnknownRoute: (RouteSettings settings) {
         return MaterialPageRoute(
           builder: (BuildContext context) => UnknownPage(),
